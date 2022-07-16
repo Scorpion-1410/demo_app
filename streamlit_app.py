d1139467c474449a055e0180cf8061b6394ee6b1
Scorpion <46865902+Scorpion-1410@users.noreply.github.com>
2022-07-16 22:08:54 +0530

    Update streamlit_app.py
    Commented unwanted line

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,6 @@ import streamlit
 import pandas
 
 
-
 streamlit.title('My Restaurant')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
@@ -26,7 +25,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+#streamlit.text(fruityvice_response.json())
 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
